2152d5320e35c29328e3ca06337e85eed9af4392
Hong_FE <ghddmlgus3@naver.com>
2021-09-03 16:43:39 +0900

    config change

--- component/sidebar/SidebarMenu_container.js
+++ component/sidebar/SidebarMenu_container.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import SidebarMenu from "./SidebarMenu";
-import ToggleIcon from "../common/ToggleIcon";
+import ToggleIcon from "../common/toggleIcon";
 import styles from "./scss/sideBarMenu_container.module.scss";
 
 const SideBarContainer = () => {
