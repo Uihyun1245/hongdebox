e5642f2ccb58af8ecf1900c713b3cecfad686301
Hong_FE <ghddmlgus3@naver.com>
2021-10-05 15:52:34 +0900

    pandacorporation cueat page v2021.10.05

--- component/pandac/common/Pandac_linkBtn.js
+++ component/pandac/common/Pandac_linkBtn.js
@@ -2,7 +2,7 @@ import styles from './scss/pandac_linkBtn.module.scss';
 const PandacLinkBtn = ({ link, text, serviceName }) => {
   return (
     <div className={styles["linkBtn-container"]}>
-      <a className={serviceName} href={link}>{text}</a>
+      <a className={styles[serviceName]} href={link}>{text}</a>
     </div>
   );
 };
